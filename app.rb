0fb7fd67dd8122669a3e06ed3a5512f8b88a8459
Veronica Bustabad <vbustabad@gmail.com>
2018-07-08 18:49:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Base
     array << params[:word4]
     array << params[:word5]
     array
-    array.push(".").join(" ")
+    array.push(".")
   end
 
   get '/:operation/:number1/:number2' do
