cd786143600660a6133af1c31baeb08ca243b6a9
Veronica Bustabad <vbustabad@gmail.com>
2018-07-08 18:45:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,7 +24,6 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     array = []
 
-    binding.pry
     array << params[:word1]
     array << params[:word2]
     array << params[:word3]
