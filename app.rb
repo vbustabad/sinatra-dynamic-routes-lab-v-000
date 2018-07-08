31fee40b42ce896779af9dec111edbaa41f9fc4a
Veronica Bustabad <vbustabad@gmail.com>
2018-07-08 15:13:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,37 @@
+require 'pry'
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
+
+  get '/square/:number' do
+    square = params[:number].to_i**2
+    square.to_s
+  end
+
+  get '/say/:number/:phrase' do
+    params[:number].to_i.times do
+      params[:phrase]
+    end
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    string_of_words = "params[:word1].gsub(/""/, "")/params[:word2].gsub(/""/, "")/params[:word3].gsub(/""/, "")/params[:word4].gsub(/""/, "")/params[:word5].gsub(/""/, "")."
+    string_of_words.gsub("/", " ")
+  end
+
+  get '/:operation/:number1/:number2' do
+    params[:operation] = params[:number1].to_i + params[:number2].to_i
+    params[:operation].to_s
+    params[:operation] = params[:number1].to_i - params[:number2].to_i
+    params[:operation].to_s
+    params[:operation] = params[:number1].to_i * params[:number2].to_i
+    params[:operation].to_s
+    params[:operation] = params[:number1].to_i / params[:number2].to_i
+    params[:operation].to_s
+  end
 
 end
