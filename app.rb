e3b13d13b2c13b262217e6b0ce4ab059e93c68ab
Veronica Bustabad <vbustabad@gmail.com>
2018-07-08 17:02:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
     params[:number].to_i.times do
       array << params[:phrase]
     end
-    array.to_s
+    array.join
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
