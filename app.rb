53808968360078945484bc446ca53cba48a922de
Veronica Bustabad <vbustabad@gmail.com>
2018-07-08 18:28:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,7 +23,6 @@ class App < Sinatra::Base
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     string_of_words = "params[:word1].gsub(/""/, "")/params[:word2].gsub(/""/, "")/params[:word3].gsub(/""/, "")/params[:word4].gsub(/""/, "")/params[:word5].gsub(/""/, "")."
-    binding.pry
     string_of_words.gsub("/", " ")
   end
 
